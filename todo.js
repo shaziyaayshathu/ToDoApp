52162b514411aa2a4d3544a22cae20af00abfb6b
shaziyaayshathu <112325398+shaziyaayshathu@users.noreply.github.com>
2022-09-30 19:01:48 +0530

    changes

---
+++ todo.js
@@ -0,0 +1,8 @@
+const uname="admin";
+const pswd="12345";
+var loguname=document.getElementById("uname");
+var logpswd=document.getElementById("password");
+function login()
+{
+    Validate(loguname,logpswd,)
+}
