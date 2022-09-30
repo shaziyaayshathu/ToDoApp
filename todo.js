c7fa5620757e3a2fa27823819c825a35ca231fc3
shaziyaayshathu <112325398+shaziyaayshathu@users.noreply.github.com>
2022-09-30 21:55:01 +0530

    changes

--- todo.js
+++ todo.js
@@ -1,8 +1,35 @@
 const uname="admin";
 const pswd="12345";
-var loguname=document.getElementById("uname");
-var logpswd=document.getElementById("password");
-function login()
+let loguname=document.getElementById("uname");
+let logpswd=document.getElementById("password");
+function login(check)
+ {  
+    alert(loguname.value);
+    
+    if(loguname.value==' ')
+    {
+        alert("Username can not be empty !");
+        return false;
+    }
+    else if(logpswd.value==' ')
+    {
+        alert("Password can not be empty !");
+        return false;
+    }
+    else
+        check();
+} 
+    
+
+
+function checkCredentials()
 {
-    Validate(loguname,logpswd,)
+    if(loguname.value=='admin' && logpswd.value=='12345')
+    return true;
+    else
+    {
+        alert("Wrong Credentials !");
+        return false;
+    }
+
 }
